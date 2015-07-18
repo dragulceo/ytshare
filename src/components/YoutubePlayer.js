fd3625f7ce564d6f9ebe79568c918ecd7bfcbaec
dragulceo <dragulceo@gmail.com>
2015-07-18 22:53:09 +0300

    WIP click on a link and view the video

--- src/components/YoutubePlayer.js
+++ src/components/YoutubePlayer.js
@@ -1,20 +1,23 @@
 'use strict';
 
 var React = require('react/addons');
+var Youtube = require('react-youtube');
 
 
 require('styles/YoutubePlayer.less');
 
 var YoutubePlayer = React.createClass({
-
+  propTypes: {
+    url: React.PropTypes.string
+  },
   render: function () {
     return (
         <div className="YoutubePlayer">
-          <p>Content for YoutubePlayer</p>
+          <Youtube url={this.props.url}/>
         </div>
       );
   }
 });
 
-export default YoutubePlayer; 
+export default YoutubePlayer;
 
--- src/components/YtshareApp.js
+++ src/components/YtshareApp.js
@@ -3,12 +3,11 @@
 var React = require('react/addons');
 var ReactTransitionGroup = React.addons.TransitionGroup;
 var YoutubeStore = require('../stores/YoutubeStore');
+var YoutubePlayer = require('./YoutubePlayer');
 
 // CSS
 require('normalize.css');
 require('../styles/main.css');
-
-var imageURL = require('../images/yeoman.png');
 
 var YtshareApp = React.createClass({
   getInitialState() {
@@ -23,19 +22,32 @@ var YtshareApp = React.createClass({
   _onChange() {
     this.setState(YoutubeStore.getState());
   },
-  render: function() {
-    var list = '';
-    if(this.state.data) {
+  /*eslint-disable no-unused-vars */
+  _onItemClick(url) {
+    var state = this.state;
+    state.activeUrl = url;
+    this.setState(state);
+  },
+  _getHandler: function (id) {
+    var onClick = this._onItemClick;
+    return () => onClick(id);
+  },
+  render: function () {
+    var list = '', url = '', onItemClick = this._onItemClick, getHandler = this._getHandler;
+    if (this.state.data) {
       list = this.state.data.map((item) => {
         return (
-          <li><a href={item}>{item}</a></li>
+          <li><a onClick={getHandler(item)} href="#">{item}</a></li>
         );
       });
+    }
+    if (this.state.activeUrl) {
+      url = this.state.activeUrl;
     }
     return (
       <div className='main'>
         <ReactTransitionGroup transitionName="fade">
-          <img src={imageURL} />
+          <YoutubePlayer url={url}/>
           {list}
         </ReactTransitionGroup>
       </div>
