8b53d5326e1ac5fda10391521b5043b3620f726f
dragulceo <dragulceo@gmail.com>
2015-12-13 21:09:35 +0200

    Add boostrap

--- src/components/main.js
+++ src/components/main.js
@@ -6,15 +6,16 @@ import { Router, Route } from 'react-router'
 import YtshareApp from './YtshareApp'
 
 // let yeomanImage = require('../images/yeoman.png');
+require('../styles/boostrap.less');
 
 class AppComponent extends React.Component {
   render() {
     return (
       <Router>
-	    <Route path="/" component={YtshareApp}>
-	    </Route>
-	  </Router>
-    );
+        <Route path="/" component={YtshareApp}>
+        </Route>
+      </Router>
+      );
   }
 }
 
